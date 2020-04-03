f427db552fe743f939edb470f4643f78efe37400
NeeleshRoy <neelesh.roy@bestbuy.com>
2020-04-03 21:34:47 +0530

    Base sass

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React from 'react';
 import { Nav } from './components';
 import { routes } from './components/routes';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+
+import './sass/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Nav routes={routes} />
-        <Switch>
-          {
-            routes.map(route => {
-              return route.name === 'Home' ?
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  component={route.component}
-                  exact
-                /> :
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  component={route.component}
-                />
-            })
-          }
-        </Switch>
-      </Router>
+      <div className="learnly-header">
+        <div className="logo">
+          <Image
+            src='./static/images/learnly-logo.png'
+            alt='learnly logo'
+            tab-index="-1"
+          />
+        </div>
+        <Router>
+          <Nav routes={routes} />
+          <Switch>
+            {
+              routes.map(route => {
+                return route.name === 'Home' ?
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    component={route.component}
+                    exact
+                  /> :
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    component={route.component}
+                  />
+              })
+            }
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export const Home = () => (
-    <h1>Home</h1>
+export const Home = (props) => (
+    <h1>This is home</h1>
 )
